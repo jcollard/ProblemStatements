a58a435fbd0e9d7464022a31629cf6b95eb4f4a9
Joseph Collard <jcollard@chadwickschool.org>
2022-03-26 15:14:07 -0700

    chore: Add MineSweeper puzzles.

---
+++ MineSweeper/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace MineSweeper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                char[,] grid = GenerateGrid(4 << i, .25, 42 + i);
+                File.WriteAllText($"test_{i}.txt", GridToString(grid));
+                File.WriteAllText($"test_{i}_solution.txt", GridToString(FillGrid(grid)));
+
+                grid = GenerateGrid(4 << i, .25, 142 + i);
+                File.WriteAllText($"validator_{i}.txt", GridToString(grid));
+                File.WriteAllText($"validator_{i}_solution.txt", GridToString(FillGrid(grid)));
+                
+            }
+
+        }
+
+        static string GridToString(char[,] grid)
+        {
+            StringBuilder b = new StringBuilder();
+            int N = grid.GetLength(0);
+            for (int r = 0; r < N; r++)
+            {
+                for (int c = 0; c < N; c++)
+                {
+                    b.Append(grid[r, c]);
+                }
+                b.Append("\n");
+            }
+            return b.ToString().Trim();
+        }
+
+        static char[,] GenerateGrid(int N, double density, int seed = 42)
+        {
+            char[,] grid = new char[N, N];
+            Random gen = new Random();
+            for (int r = 0; r < N; r++)
+            {
+                for (int c = 0; c < N; c++)
+                {
+                    grid[r, c] = gen.NextDouble() < density ? 'b' : 'o';
+                }
+            }
+            return grid;
+        }
+
+        static char[,] FillGrid(char[,] grid)
+        {
+            int N = grid.GetLength(0);
+            char[,] result = new char[N, N];
+            for (int r = 0; r < N; r++)
+            {
+                for (int c = 0; c < N; c++)
+                {
+                    result[r, c] = grid[r, c] == 'o' ? CountNeighbors(r, c, grid) : 'b';
+                }
+            }
+            return result;
+        }
+
+        static char CountNeighbors(int r, int c, char[,] grid)
+        {
+            int N = grid.GetLength(0);
+            (int, int)[] locs =
+            {
+                (r - 1, c - 1), (r - 1, c), (r - 1, c + 1),
+                (r, c - 1), (r, c + 1),
+                (r + 1, c - 1), (r + 1, c), (r + 1, c + 1)
+            };
+            int count = locs.Where(((int r, int c) pair) => pair.r >= 0 && pair.r < N && pair.c >= 0 && pair.c < N && grid[pair.r, pair.c] == 'b').Count();
+
+            return $"{count}"[0];
+        }
+    }
+}
---
+++ MineSweeper/Statement.md
@@ -0,0 +1,46 @@
+# Title
+
+Mine Sweeper
+
+# Statement
+You are given an N x N field which contains bombs 'b' and clear cells 'o'. Your task is to replace each clear cell with the number of bombs that it's surrounded and print the result.
+
+
+# Input description
+<<Line 1:>> An integer [[N]], the size of matrix.
+<<Next [[N]] lines:>> Elements of the row separated by spaces( 'o' or 'b' ).
+
+# Output description
+<<[[N]] lines:>> Print the modified matrix.
+
+# Constraints
+2 <= [[N]] <= 20
+
+# Test 1
+Input
+
+Output
+
+# Validator 1
+
+
+# Test 2
+
+# Validator 2
+
+# Test 3
+
+# Validator 3
+
+# Test 4
+
+# Validator 4
+
+# Solution 
+
+
+# Generator
+
+read N:int
+loop N read GAME:string(6)
+write 5 13 18 26 31 32 34 40 47 54
